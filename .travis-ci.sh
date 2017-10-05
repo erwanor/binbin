d100beb0bc8b079e8f4ade9c01fa598f4a9c2fd1
Aaron <aaron.ounn@gmail.com>
2017-10-05 03:19:13 +0200

    Fix env var typo in TravisCI script

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -5,7 +5,7 @@ ppa=avsm/ocaml42+opam120
 echo "yes" | sudo add-apt-repository ppa:$ppa
 sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
-export OPMAYES=1
+export OPAMYES=1
 opam init
 opam install ${OPAM_DEPENDS}
 eval `opam config env`
