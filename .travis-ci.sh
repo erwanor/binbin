decff69b99a1c08c82365e77f9831ad9304563f6
Aaron <aaron.ounn@gmail.com>
2017-10-05 04:00:14 +0200

    Add jbuilder to OPAM deps + update ppa

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,12 +1,14 @@
-OPAM_DEPENDS="ounit"
+OPAM_DEPENDS="ounit jbuilder"
 
-ppa=avsm/ocaml42+opam120
-
+ppa=avsm/ocaml42+opam12
 echo "yes" | sudo add-apt-repository ppa:$ppa
+
 sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
+
 export OPAMYES=1
 opam init
+opam update
 opam install ${OPAM_DEPENDS}
 eval `opam config env`
 make build
